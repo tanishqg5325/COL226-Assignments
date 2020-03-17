bbcd9d7e24ada2e245039b11c59f5ba96ac8f260
Tanishq Gupta <tanishqg2406@gmail.com>
2020-03-17 15:40:39 +0530

    added comments

--- A5/a5.ml
+++ A5/a5.ml
@@ -5,11 +5,17 @@ type substitution = (variable * term) list
 
 exception NOT_UNIFIABLE
 
+(* Given an element x and a list y, checks whether x is present in y *)
 let rec exists x y = match y with
     [] -> false
   | z::ys -> (x = z) || (exists x ys)
 ;;
 
+(*
+ * Given a signature consisting of symbols and their arities (>= 0) as a list of
+ * (symbol, arity) pairs, checks whether the signature is a valid signature (no
+ * repeated symbols, arities are non-negative etc.) 
+ *)
 let rec check_sig (signature:(symbol * int) list): bool =
   match signature with
       [] -> true
@@ -20,24 +26,34 @@ let rec check_sig (signature:(symbol * int) list): bool =
         in (arity >= 0) && (not (repeats s xs)) && (check_sig xs)
 ;;
 
+(*
+ * Given a valid signature (checked using check_sig), wfterm checks that a given
+ * term is well-formed according to the signature
+ *)
 let rec wfterm (signature:(symbol * int) list) (t:term): bool =
   match t with
       V(v) -> true
     | Node(s, l) -> exists (s, List.length l) signature && List.for_all (wfterm signature) l
 ;;
 
+(* Given a well-formed term, return its height *)
 let rec ht (t:term): int =
   match t with
       V(v) -> 0
     | Node(s, l) -> 1 + (List.fold_left max (-1) (List.map ht l))
 ;;
 
+(* Given a well-formed term, return its size *)
 let rec size (t:term): int =
   match t with
       V(v) -> 1
     | Node(s, l) -> 1 + (List.fold_left (+) 0 (List.map size l))
 ;;
 
+(*
+ * Given a well-formed term, return set (represented as a list with no duplicates)
+ * of variables appearing in it
+ *)
 let rec vars (t:term): variable list =
   match t with
       V(v) -> [v]
@@ -49,21 +65,30 @@ let rec vars (t:term): variable list =
         in List.fold_left union [] (List.map vars l)
 ;;
 
-let rec subst (t:term) (s:substitution): term =
+(*
+ * Given a term t and a substitution s, applies the (Unique Homomorphic
+ * Extension of) s to t
+ *)
+let subst (t:term) (s:substitution): term =
   let rec substOne (subst_vt:variable * term) (t_:term): term =
     match t_ with
         V(x) -> if x = fst subst_vt then snd subst_vt
                 else t_
       | Node(s, l) -> Node(s, List.map (substOne subst_vt) l)
-  in List.fold_right substOne s t
+  in List.fold_left (fun t vt -> substOne vt t) t s
 ;;
 
+(* Given a variable v, checks whether v is present in well-formed term t *)
 let rec variableInTerm (v:variable) (t:term): bool =
   match t with
       V(x) -> x = v
     | Node(s, l) -> List.exists (variableInTerm v) l
 ;;
 
+(*
+ * Given two well-formed terms t1 and t2, returns their most general unifier,
+ * if it exists and otherwise raises an exception NOT_UNIFIABLE
+ *)
 let rec mgu (t1:term) (t2:term): substitution =
   match (t1, t2) with
       (V(x), V(y)) -> if x = y then []
@@ -75,6 +100,6 @@ let rec mgu (t1:term) (t2:term): substitution =
     | (Node(s1, l1), Node(s2, l2)) -> 
         if s1 <> s2 then raise NOT_UNIFIABLE
         else 
-          let f s tt = (mgu (subst (fst tt) s) (subst (snd tt) s)) @ s in
+          let f s tt = s @ (mgu (subst (fst tt) s) (subst (snd tt) s)) in
           List.fold_left f [] (List.combine l1 l2)
 ;;
