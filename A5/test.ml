e01e34af1aabf5b9bd6505e598c49ea4f59310b0
Tanishq Gupta <tanishqg2406@gmail.com>
2020-03-17 23:06:44 +0530

    testing of mgu remains

--- A5/test.ml
+++ A5/test.ml
@@ -1,2 +1,13 @@
-let sgn = [("sef", 2); ("casc", 0); ("seft", 2); ("casc", 4)];;
+let sgn = [("plus", 2); ("mult", 2); ("zero", 0); ("one", 0); ("two", 0); ("unary", 1)];;
 check_sig sgn;;
+
+let t = Node("plus", [Node("mult", [V("a"); Node("one", [])]); V("b")]);;
+wfterm sgn t;;
+ht t;;
+size t;;
+vars t;;
+let s = [("b", Node("plus", [Node("one", []); V("a")])); ("a", Node("unary", [V("c")])); ("c", Node("two", []))];;
+subst s t;;
+let t1 = Node("plus", [Node("zero", []); V("a")]);;
+let t2 = Node("plus", [V("a"); Node("zero", [])]);;
+mgu t1 t2;;
