85daf37b03178acdcf2c6f1c16aae4e583c20169
Tanishq Gupta <tanishqg2406@gmail.com>
2020-03-29 22:17:21 +0530

    some progress

--- A6/interpreter.ml
+++ A6/interpreter.ml
@@ -50,11 +50,23 @@ let rec getSigTerm (sign:signature) (t:term): signature = match t with
 let getSigAtom (sign:signature) (A(s, l)): signature = getSigTerm sign (Node(s, l))
 ;;
 
-let rec isValidProgram (prog:program) (sign: (symbol*int) list): bool = match prog with
-    [] -> true
-  | (F(H(a)))::xs -> isValidProgram xs (getSigAtom sign a)
-  | (R(H(a), B(l)))::xs -> let f a = F(H(a)) in
-                          isValidProgram ((map f (a::l)) @ xs) sign
+let rec getSigProgram (prog:program) (sign:signature): signature = match prog with
+    [] -> sign
+  | (F(H(a)))::xs -> getSigProgram xs (getSigAtom sign a)
+  | (R(H(a), B(l)))::xs ->  let f a = F(H(a)) in
+                            getSigProgram ((map f (a::l)) @ xs) sign
+;;
+
+let rec wfterm (sign:signature) (t:term): bool =
+  match t with
+      V(v) -> true
+    | Node(s, l) -> exists (s, List.length l) sign && foldl (&&) true (map (wfterm sign) l)
+;;
+
+let rec wfgoal (g:goal) (sign:signature): bool =
+  match g with
+      G([]) -> true
+    | G(A(s, l)::xs) -> wfterm sign (Node(s, l)) && wfgoal (G(xs)) sign
 ;;
 
 let rec subst (s:substitution) (t:term): term =
@@ -65,13 +77,16 @@ let rec subst (s:substitution) (t:term): term =
                 | s'::xs -> if fst s' = x then snd s' else subst xs t
 ;;
 
+let rec subst_atom (s:substitution) (A(s', l)) = A(s', map (subst s) l)
+;;
+
 let rec variableInTerm (v:variable) (t:term): bool =
   match t with
       V(x) -> x = v
     | Node(s, l) -> foldl (||) false (map (variableInTerm v) l)
 ;;
 
-let rec compose (s1:substitution) (s2:substitution): substitution =
+let compose (s1:substitution) (s2:substitution): substitution =
   let f s x = (fst x, subst s (snd x)) in (map (f s2) s1) @ s2
 ;;
 
@@ -93,3 +108,26 @@ let rec mgu_term (t1:term) (t2:term): substitution =
 let mgu_atom (A(s1, l1)) (A(s2, l2)): substitution = mgu_term (Node(s1, l1)) (Node(s2, l2))
 ;;
 
+let solve_atom_atom a1 a2 unif =
+  try (true, compose unif (mgu_atom (subst_atom unif a1) (subst_atom unif a2)))
+  with NOT_UNIFIABLE -> (false, [])
+;;
+
+let rec solve_atom_clause prog a clause unif = match clause with
+    F(H(a')) -> solve_atom_atom a a' unif
+  | R(H(a'), B(al)) -> match (solve_atom_atom a a' unif) with
+       (true, u) -> solve_goal prog (G(al)) u
+      | _ -> (false, [])
+
+and solve_goal (prog:program) (goal:goal) (unif:substitution) = match goal with
+    G([]) -> (true, unif)
+  | G(a::gs) ->
+      let rec f prog' = match prog' with
+          [] -> (false, [])
+        | c::ps ->  match (solve_atom_clause prog a c unif) with
+                        (true, u) -> (true, u)
+                      | _ -> f ps
+      in let ans = f prog in
+      if fst ans = true then solve_goal prog (G(gs)) (snd ans)
+      else (false, [])
+;;
