63ea43a024454f20ddfa1304241d0c2f30b983a6
Tanishq Gupta <tanishqg2406@gmail.com>
2020-08-18 12:50:33 +0530

    secd complete, test remains

--- A8/secd.ml
+++ A8/secd.ml
@@ -4,23 +4,33 @@ type exp = Num of int | Bool of bool
          | Var of var
          | Plus of exp * exp | Times of exp * exp
          | Cond of exp * exp * exp
-         | Invoke of exp * exp
          | Lambda of var * exp
-
-type answer = N of int | B of bool | VClos of environment * var * exp
-and
-environment = (var * answer) list
+         | Invoke of exp * exp
 
 type opcode = NUM of int | BOOL of bool 
             | VAR of string 
             | PLUS | TIMES
             | COND of opcode list * opcode list
             | CLOS of string * opcode list | RET
-            | APP
+            | INVOKE
+
+type code = opcode list
+
+type answer = N of int | B of bool | VClos of environment * var * code
+and
+environment = (var * answer) list
 
 type stack = answer list
-type code = opcode list
 type dump = (stack * environment * code) list
+
+exception BadState
+exception VarNotFound
+
+
+let rec lookup (e:environment) (v:var): answer = match e with
+    [] -> raise VarNotFound
+  | (x, a)::es -> if v = x then a else lookup es v
+;;
 
 
 let rec compile (e:exp): opcode list = match e with
@@ -30,10 +40,22 @@ let rec compile (e:exp): opcode list = match e with
   | Plus(e1, e2) -> (compile e1) @ (compile e2) @ [PLUS]
   | Times(e1, e2) -> (compile e1) @ (compile e2) @ [TIMES]
   | Cond(e1, e2, e3) -> (compile e1) @ [COND((compile e2), (compile e3))]
-  | Invoke(e1, e2) -> (compile e1) @ (compile e2) @ [APP]
   | Lambda(V(x), e') -> [CLOS(x, (compile e') @ [RET])]
+  | Invoke(e1, e2) -> (compile e1) @ (compile e2) @ [INVOKE]
 ;;
 
 
-let rec execute (s:stack) (e:environment) (c:code) (d:dump): answer =
-
+let rec execute (s:stack) (e:environment) (c:code) (d:dump): answer = match (s, e, c, d) with
+    ([a], _, [], []) -> a
+  | (_, _, NUM(n)::c', _) -> execute (N(n)::s) e c' d
+  | (_, _, BOOL(b)::c', _) -> execute (B(b)::s) e c' d
+  | (_, _, VAR(x)::c', _) -> execute ((lookup e (V(x)))::s) e c' d
+  | ((N(n2)::N(n1)::s'), _, PLUS::c', _) -> execute (N(n1+n2)::s') e c' d
+  | ((N(n2)::N(n1)::s'), _, TIMES::c', _) -> execute (N(n1*n2)::s') e c' d
+  | ((B(true)::s'), _, COND(c1, _)::c', _) -> execute s' e (c1 @ c') d
+  | ((B(false)::s'), _, COND(_, c2)::c', _) -> execute s' e (c2 @ c') d
+  | (_, _, CLOS(x, c1)::c', _) -> execute ((VClos(e, V(x), c1))::s) e c' d
+  | ([a], _, [RET], (s', e', c')::d') -> execute (a::s') e' c' d'
+  | ((a::(VClos(e', v, c1))::s'), _, INVOKE::c', _) -> execute [] ((v, a)::e') c1 ((s', e, c')::d)
+  | _ -> raise BadState
+;;
