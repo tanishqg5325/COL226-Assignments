84c6a196787f7a455805f2490d5dbd33f3789e20
Tanishq Gupta <tanishqg2406@gmail.com>
2020-08-18 23:37:13 +0530

    added comparison with 0

--- A8/krivine.ml
+++ A8/krivine.ml
@@ -3,6 +3,7 @@ type var = V of string
 type exp = Num of int | Bool of bool
          | Var of var
          | Plus of exp * exp | Times of exp * exp
+         | CompWith0 of exp
          | Cond of exp * exp * exp
          | Lambda of var * exp
          | Invoke of exp * exp
@@ -35,6 +36,11 @@ let mul (c1: closure) (c2: closure): exp = match (c1, c2) with
   | _ -> raise BadState
 ;;
 
+let comp0 (c:closure): exp = match c with
+    Clos(_, Num(n)) -> Bool(n = 0)
+  | _ -> raise BadState
+;;
+
 let ite (c:closure): bool = match c with
     Clos(_, Bool(b)) -> b
   | _ -> raise BadState
@@ -46,6 +52,7 @@ let rec execute (c:closure) (s:stack): closure = match (c, s) with
   | Clos(t, Var(v)), _            -> execute (lookup t v) s
   | Clos(t, Plus(e1, e2)), _      -> Clos(t, add (execute (Clos(t, e1)) s) (execute (Clos(t, e2)) s))
   | Clos(t, Times(e1, e2)), _     -> Clos(t, mul (execute (Clos(t, e1)) s) (execute (Clos(t, e2)) s))
+  | Clos(t, CompWith0(e)), _      -> Clos(t, comp0 (execute (Clos(t, e)) s))
   | Clos(t, Cond(e1, e2, e3)), _  -> if ite (execute (Clos(t, e1)) s) then execute (Clos(t, e2)) s else execute (Clos(t, e3)) s
   | Clos(_, Lambda(_, _)), []     -> c
   | Clos(t, Lambda(v, e)), c'::s' -> execute (Clos((v, c')::t, e)) s'
--- A8/secd.ml
+++ A8/secd.ml
@@ -3,6 +3,7 @@ type var = V of string
 type exp = Num of int | Bool of bool
          | Var of var
          | Plus of exp * exp | Times of exp * exp
+         | CompWith0 of exp
          | Cond of exp * exp * exp
          | Lambda of var * exp
          | Invoke of exp * exp
@@ -10,6 +11,7 @@ type exp = Num of int | Bool of bool
 type opcode = NUM of int | BOOL of bool 
             | VAR of string 
             | PLUS | TIMES
+            | COMP
             | COND of opcode list * opcode list
             | CLOS of string * opcode list | RET
             | INVOKE
@@ -39,18 +41,20 @@ let rec compile (e:exp): opcode list = match e with
   | Var(V(s))           -> [VAR(s)]
   | Plus(e1, e2)        -> (compile e1) @ (compile e2) @ [PLUS]
   | Times(e1, e2)       -> (compile e1) @ (compile e2) @ [TIMES]
+  | CompWith0(e')       -> (compile e') @ [COMP]
   | Cond(e1, e2, e3)    -> (compile e1) @ [COND((compile e2), (compile e3))]
   | Lambda(V(x), e')    -> [CLOS(x, (compile e') @ [RET])]
   | Invoke(e1, e2)      -> (compile e1) @ (compile e2) @ [INVOKE]
 ;;
 
 let rec execute (s:stack) (e:environment) (c:code) (d:dump): answer = match (s, c, d) with
-    ([a], [], []) -> a
+    ([a], [], [])                                -> a
   | (_, NUM(n)::c', _)                           -> execute (N(n)::s) e c' d
   | (_, BOOL(b)::c', _)                          -> execute (B(b)::s) e c' d
   | (_, VAR(x)::c', _)                           -> execute ((lookup e (V(x)))::s) e c' d
   | ((N(n2)::N(n1)::s'), PLUS::c', _)            -> execute (N(n1+n2)::s') e c' d
   | ((N(n2)::N(n1)::s'), TIMES::c', _)           -> execute (N(n1*n2)::s') e c' d
+  | ((N(n)::s'), COMP::c', _)                    -> if n = 0 then execute (B(true)::s') e c' d else execute (B(false)::s') e c' d
   | ((B(true)::s'), COND(c1, _)::c', _)          -> execute s' e (c1 @ c') d
   | ((B(false)::s'), COND(_, c2)::c', _)         -> execute s' e (c2 @ c') d
   | (_, CLOS(x, c1)::c', _)                      -> execute ((VClos(e, V(x), c1))::s) e c' d
@@ -65,3 +69,5 @@ let secd (e:environment) (ex:exp): answer = execute [] e (compile ex) []
 
 secd [] (Invoke(Lambda(V("x"), Times(Var(V("x")), Num(5))), Plus(Num(2), Num(4))));;
 secd [(V("f"), VClos([(V("y"), N(2))], V("x"), (compile (Plus(Var(V("x")), Var(V("y"))))) @ [RET]))] (Invoke(Var(V("f")), Num(5)));;
+let sq = secd [] (Lambda(V("x"), Times(Var(V("x")), Var(V("x")))));;
+secd [(V("sq"), sq)] (Invoke(Var(V("sq")), Num(37)));;
